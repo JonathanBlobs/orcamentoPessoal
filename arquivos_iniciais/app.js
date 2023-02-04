1bd969922a131bc3e586cb10c47c5307f28cfc1f
Jonathan Alves <jonathan.silva.300795@gmail.com>
2023-02-04 16:04:20 -0300

    cadastrar novos itens

--- arquivos_iniciais/app.js
+++ arquivos_iniciais/app.js
@@ -1,5 +1,5 @@
-class despesa {
-  constructor(ano, mes, dia, tipo, descricao, valor){
+class Despesa {
+  constructor(ano, mes, dia, tipo, descricao, valor) {
     this.ano = ano
     this.mes = mes
     this.dia = dia
@@ -7,12 +7,57 @@ class despesa {
     this.descricao = descricao
     this.valor = valor
   }
+
+  validarDados(){
+
+    for(let i in this){
+      
+    }
+
+  }
+
 }
+
+class Bd {
+
+  constructor() {
+
+    let id = localStorage.getItem('id')
+
+    if (id === null) {
+
+      localStorage.setItem('id', 0)
+
+    }
+
+  }
+
+  getProximoId() {
+
+    let proximoId = localStorage.getItem('id')
+    return parseInt(proximoId) + 1
+
+  }
+
+  gravar(d) {
+
+    let id = this.getProximoId()
+
+    localStorage.setItem(id, JSON.stringify(d))
+
+    localStorage.setItem('id', id)
+
+  }
+
+}
+
+let bd = new Bd()
+
 
 function cadastrarDespesa() {
 
-  let ano =document.getElementById('ano')
-  let mes = document.getElementById('mes').value
+  let ano = document.getElementById('ano')
+  let mes = document.getElementById('mes')
   let dia = document.getElementById('dia')
 
   let tipo = document.getElementById('tipo')
@@ -28,4 +73,14 @@ function cadastrarDespesa() {
     valor.value
   )
 
+  if(despesa.validarDados()){
+
+  bd.gravar(despesa)
+
+  } else {
+
+
+  }  
+
 }
+
